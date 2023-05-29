8467a74dbc4bd972287bd1fe1ca53488d2be2e1d
LukasRoeding <goskodovesche@gmail.com>
2023-05-29 14:25:55 +0200

    player class and drawing player object

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
 const canvas = document.querySelector('canvas');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 const context = canvas.getContext('2d');
-console.log(context)
+
+class Player {
+    constructor() {
+        this.position = {
+            x: 100,
+            y: 100
+        }
+        this.width = 100
+        this.height = 100
+    }
+
+    draw() {
+        context.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+}
+
+const player = new Player();
+player.draw();
+
+console.log(player)
