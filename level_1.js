599180d4d6d17b64ce4ecb7a4cd732362cab195b
LukasRoeding <goskodovesche@gmail.com>
2023-05-29 22:15:24 +0200

    rough lvldata

--- level_1.js
+++ level_1.js
@@ -1,4 +1,24 @@
 export const level = {
+    deathPits: [
+        {
+            y: 800,
+            width: 200
+        }
+    ],
+    informations: [
+        {
+            x: 100,
+            y: 200,
+            html: '<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut</p>'
+        }
+    ],
+    doors: [
+        {
+            x: 1000,
+            y: 1000,
+            target: '/level_1'
+        }
+    ],
     platforms: [
         {
             x: 100,
