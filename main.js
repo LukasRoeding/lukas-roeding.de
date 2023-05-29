e557c646f719b41550d38132e5bc04b7ae7846e5
LukasRoeding <goskodovesche@gmail.com>
2023-05-29 14:41:17 +0200

    gravity

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 const canvas = document.querySelector('canvas');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+
 const context = canvas.getContext('2d');
+
+const gravity = 1;
 
 class Player {
     constructor() {
@@ -9,16 +12,40 @@ class Player {
             x: 100,
             y: 100
         }
-        this.width = 100
-        this.height = 100
+        this.velocity = {
+            x: 0,
+            y: 1
+        }
+        this.width = 30
+        this.height = 30
     }
 
     draw() {
+        context.fillStyle = 'red'
         context.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+
+    update() {
+        this.draw();
+        this.position.y += this.velocity.y;
+
+        if(this.position.y + this.height + this.velocity.y <= canvas.height) {
+            this.velocity.y += gravity;
+        } else {
+            this.velocity.y = 0;
+        }
+        
     }
 }
 
 const player = new Player();
-player.draw();
+player.update();
 
 console.log(player)
+function animate() {
+    requestAnimationFrame(animate)
+    context.clearRect(0,0, canvas.width, canvas.height);
+    player.update();
+}
+
+animate()
