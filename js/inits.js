f8bf474d27f9fc28533cd9b0c97635f52b27f0e6
LukasRoeding <goskodovesche@gmail.com>
2023-05-30 22:26:08 +0200

    fix properly resetting platforms when death

--- js/inits.js
+++ js/inits.js
@@ -3,7 +3,8 @@ export function init(player, platforms, images, level) {
     player.position.x = 100
     player.position.y = 200
     for (let index = 0; index < platforms.length; index++) {
-        platforms[index].position = level.platforms[index]  
+        platforms[index].position.x = level.platforms[index].x
+        platforms[index].position.y = level.platforms[index].y
     }
     for (let index = 0; index < images.length; index++) {
         images[index].position.x = level.images[index].x
