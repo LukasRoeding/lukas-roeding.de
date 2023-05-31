ec4ff4facb1af38928baa7c974e1b6ce6e9f592b
LukasRoeding <goskodovesche@gmail.com>
2023-05-31 13:57:56 +0200

    slightly optimized collision detection for information blocks, only check x coordinates once instead of potentially twice

--- js/collision.js
+++ js/collision.js
@@ -13,21 +13,21 @@ export function collision(platforms, player, informations) {
     informations.forEach(information => {
         information.draw();
         if (
-            player.position.y + player.height <= information.position.y && 
-            player.position.y + player.height + player.velocity.y >= information.position.y &&
             player.position.x + player.width >= information.position.x &&
             player.position.x <= information.position.x + information.width
         ) {
-            player.velocity.y = 0
-        }
-        if (
-            player.position.y >= information.position.y + information.height && 
-            player.position.y + player.velocity.y <= information.position.y + information.height &&
-            player.position.x + player.width >= information.position.x &&
-            player.position.x <= information.position.x + information.width
-        ) {
-            information.displayInformation()
-            player.velocity.y = 0
+            if (
+                player.position.y + player.height <= information.position.y && 
+                player.position.y + player.height + player.velocity.y >= information.position.y
+            ) {
+                player.velocity.y = 0
+            } else if (
+                player.position.y >= information.position.y + information.height && 
+                player.position.y + player.velocity.y <= information.position.y + information.height
+            ) {
+                information.displayInformation()
+                player.velocity.y = 0
+            }
         }
     });
 }
