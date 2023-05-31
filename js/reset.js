a3353bfc1a1aca4d058e2a335750be3b138cc12d
LukasRoeding <goskodovesche@gmail.com>
2023-05-31 17:11:48 +0200

    adding informations to the reset function

--- js/reset.js
+++ js/reset.js
@@ -1,4 +1,4 @@
-export function reset(player, platforms, images, level) {
+export function reset(player, platforms, images, informations, level) {
     player.position.x = 100
     player.position.y = 200
     for (let index = 0; index < platforms.length; index++) {
@@ -8,5 +8,9 @@ export function reset(player, platforms, images, level) {
     for (let index = 0; index < images.length; index++) {
         images[index].position.x = level.images[index].x
         images[index].position.y = level.images[index].y
+    }
+    for (let index = 0; index < informations.length; index++) {
+        informations[index].position.x = level.informations[index].x
+        informations[index].position.y = level.informations[index].y
     }
 }
