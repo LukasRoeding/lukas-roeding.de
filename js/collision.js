4f32acaeed2eb69cf867f016f3df7e4437438d47
LukasRoeding <goskodovesche@gmail.com>
2023-05-31 13:52:20 +0200

    check if player jumps against bottom of information

--- js/collision.js
+++ js/collision.js
@@ -10,16 +10,24 @@ export function collision(platforms, player, informations) {
             player.velocity.y = 0
         }
     });
-    informations.forEach(platform => {
-        platform.draw();
+    informations.forEach(information => {
+        information.draw();
         if (
-            player.position.y + player.height <= platform.position.y && 
-            player.position.y + player.height + player.velocity.y >= platform.position.y &&
-            player.position.x + player.width >= platform.position.x &&
-            player.position.x <= platform.position.x + platform.width
+            player.position.y + player.height <= information.position.y && 
+            player.position.y + player.height + player.velocity.y >= information.position.y &&
+            player.position.x + player.width >= information.position.x &&
+            player.position.x <= information.position.x + information.width
         ) {
             player.velocity.y = 0
-            platform.displayInformation()
+        }
+        if (
+            player.position.y >= information.position.y + information.height && 
+            player.position.y + player.velocity.y <= information.position.y + information.height &&
+            player.position.x + player.width >= information.position.x &&
+            player.position.x <= information.position.x + information.width
+        ) {
+            information.displayInformation()
+            player.velocity.y = 0
         }
     });
 }
