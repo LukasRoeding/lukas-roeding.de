1a367568c6547f3bd88d3b886710333ab25dd5d6
LukasRoeding <goskodovesche@gmail.com>
2023-06-02 12:12:48 +0200

    creating an offscreencanvas, so images dont have to be resized every frame

--- entities/image.js
+++ entities/image.js
@@ -4,11 +4,16 @@ export class Image extends Entity {
     constructor(position, context, canvas, image, width, height) {
         super({x:position.x, y:position.y}, width, height, context, canvas)
         this.image = image
+        this.offscreenCanvas = document.createElement('canvas');
+        this.offscreenCanvas.width = innerWidth;
+        this.offscreenCanvas.height = innerHeight;
+        const ctx = this.offscreenCanvas.getContext("2d");
+        ctx.drawImage(this.image, this.position.x, this.position.y, this.width, this.height)  
     }
 
     draw() {
         if (this.position.x <= innerWidth || this.position.x + this.width >= 0) {
-          this.context.drawImage(this.image, this.position.x, this.position.y, this.width, this.height)  
+          this.context.drawImage(this.offscreenCanvas, this.position.x, this.position.y)  
         }
     }
 }
