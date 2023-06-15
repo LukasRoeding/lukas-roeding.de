ba19070b686a42a9ec8dce8cbe15a7b4872cfd63
LukasRoeding <goskodovesche@gmail.com>
2023-06-15 13:53:07 +0200

    making experience level data properly

--- levels/experience_level.js
+++ levels/experience_level.js
@@ -1,102 +1,146 @@
-export const level = {
-    informations: [
-        {
-            id: 1,
-            x: 200,
-            y: innerHeight - 200,
-            html: '<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>',
-            title: 'Willkommen',
-            link: ''
-        }
-    ],
-    doors: [
-        {
-            x: 880,
-            y: innerHeight - 200,
-            target: '/experience.html'
-        }
-    ],
-    images: [
-        {
-            id: 1,
-            x: -1,
-            y: -2,
-            w: innerWidth * 3,
-            h: innerHeight + 4,
-            source: '../images/background.png'
-        },
-        {
-            id: 2,
-            x: 0,
-            y: 100,
-            w: innerHeight * 12,
-            h: innerHeight,
-            source: '../images/hills.png'
-        },
-        {
-            id: 3,
-            x: 100,
-            y: innerHeight - 300,
-            w: 200,
-            h: 200,
-            source: '../images/wasd.png'
-        },
-    ],
-    enemies: [
-        {
-            x: 100,
-            y: 100,
-            type: 'violett_mushroom'
-        }
-    ],
-    platforms: [
-        {
-            id: 1,
-            x: -1,
-            y: innerHeight - 40,
-            height: 40,
-            width: 400,
-            source: '../images/platform.png'
-        },
-        {
-            id: 2,
-            x: 398,
-            y: innerHeight - 40,
-            height: 40,
-            width: 400,
-            source: '../images/platform.png'
-        },
-        {
-            id: 3,
-            x: 596,
-            y: innerHeight - 40,
-            height: 40,
-            width: 400,
-            source: '../images/platform.png'
-        },
-        {
-            id: 4,
-            x: 1200,
-            y: innerHeight - 40,
-            height: 40,
-            width: 400,
-            source: '../images/platform.png'
-        },
-        {
-            id: 5,
-            x: 800-4,
-            y: innerHeight - 140,
-            height: 100,
-            width: 200,
-            source: '../images/platformSmallTall.png'
-        },
-        {
-            id: 6,
-            x: 1200,
-            y: innerHeight - 140,
-            height: 100,
-            width: 200,
-            source: '../images/platformSmallTall.png'
-        },
-    ]
+export function level(height) {
+    return {
+        informations: [
+            {
+                id: 1,
+                x: height / 2,
+                y: height - height / 3,
+                html: "<p>In diesem Abschnitt der Website finden Sie Projekte an denen ich professionell mitgewirkt habe.</p>",
+                title: 'Berufserfahrung',
+                link: ''
+            }
+        ],
+        doors: [
+            {
+                x: height + height / 20,
+                y: height - height / 4 - height / 10,
+                target: '/'
+            },
+            {
+                x: height + height / 2 - height / 10 - height / 20,
+                y: height - height / 4 - height / 10,
+                target: '/education.html'
+            },
+            {
+                x: height + height / 2 + height / 20 + height / 8,
+                y: height - height / 4 - height / 10,
+                target: '/personal.html'
+            },
+            {
+                x: height * 2 - height / 10 - height / 20 + height / 8,
+                y: height - height / 4 - height / 10,
+                target: '/fun.html'
+            }
+        ],
+        images: [        
+            {
+                id: 1,
+                x: 100,
+                y: height / 2,
+                w: height / 4,
+                h: height / 4,
+                source: '../images/wasd.png'
+            },
+            {
+                id: 2,
+                x: height + height / 20 - height / 80,
+                y: height - height / 4 - height / 10 - height / 6,
+                w: height / 8,
+                h: height / 8,
+                source: '../images/experience.png'
+            },
+            {
+                id: 3,
+                x: height + height / 2 - height / 10 - height / 20 - height / 80,
+                y: height - height / 4 - height / 10 - height / 6,
+                w: height / 8,
+                h: height / 8,
+                source: '../images/education.png'
+            },
+            {
+                id: 4,
+                x: height + height / 2 + height / 20 - height / 120 + height / 8,
+                y: height - height / 4 - height / 10 - height / 6,
+                w: height / 9,
+                h: height / 9,
+                source: '../images/personal.png'
+            },
+            {
+                id: 5,
+                x: height * 2 - height / 10 - height / 20 - height / 80 + height / 8,
+                y: height - height / 4 - height / 10 - height / 6,
+                w: height / 8,
+                h: height / 9,
+                source: '../images/fun.png'
+            },
+        ],
+        backgroundImages: [
+            {
+                id: 1,
+                x: -1,
+                y: -2,
+                w: height * 10,
+                h: height + 4,
+                source: '../images/background.png'
+            },
+            {
+                id: 2,
+                x: 0,
+                y: 100,
+                w: height * 12,
+                h: height,
+                source: '../images/hills.png'
+            },
+        ],
+        enemies: [
+            {
+                x: 100,
+                y: 100,
+                type: 'violett_mushroom'
+            }
+        ],
+        platforms: [        
+            {
+                id: 1,
+                x: height - 1,
+                y: height - height / 4,
+                height: height / 4,
+                width: height / 2,
+                source: '../images/platformSmallTall.png'
+            },
+            {
+                id: 2,
+                x: height + height / 2 + height / 8 - 4,
+                y: height - height / 4,
+                height: height / 4,
+                width: height / 2,
+                source: '../images/platformSmallTall.png'
+            },
+            {
+                id: 3,
+                x: -1,
+                y: height - height / 10,
+                height: height / 10,
+                width: height,
+                source: '../images/platform.png'
+            },
+            {
+                id: 4,
+                x: height - 3,
+                y: height - height / 10,
+                height: height / 10,
+                width: height,
+                source: '../images/platform.png'
+            },
+            {
+                id: 5,
+                x: height * 2 - 6,
+                y: height - height / 10,
+                height: height / 10,
+                width: height,
+                source: '../images/platform.png'
+            },
+
+        ]
+    }
 }
