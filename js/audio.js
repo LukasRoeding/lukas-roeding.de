fd1a309437d32ae7e13c622be3682c8f7ac00f51
LukasRoeding <goskodovesche@gmail.com>
2023-06-21 12:25:56 +0200

    fire audio eventlisteners only once

--- js/audio.js
+++ js/audio.js
@@ -2,11 +2,11 @@ export function audio() {
     const audioElement = document.getElementById('background-music');
     window.addEventListener("touchstart", () => {
         audioElement.play()
-    })
+    }, { once: true })
     window.addEventListener("keydown", () => {
         audioElement.play()
-    })
+    }, { once: true })
     window.addEventListener("mousedown", () => {
         audioElement.play()
-    })
+    }, { once: true })
 }
