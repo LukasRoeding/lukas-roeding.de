5fd8f7ef93c805554a73fb64cf77823a8d146b26
LukasRoeding <goskodovesche@gmail.com>
2023-06-22 15:34:06 +0200

    this works as a level for now

--- levels/fun_level.js
+++ levels/fun_level.js
@@ -3,6 +3,18 @@ import { createImage } from "../js/createImage.js";
 export function level(height) {
     return {
         informations: [
+            {
+                id: 1,
+                x: height * 3.75,
+                y: height - height / 3,
+                html: "<p>Glückwunsch! Du hast es ins Ziel geschafft!</p>" +
+                "<p>Das wars fürs erste mit der Website. Ich werde bestimmt in der Zukunft weiter dran arbeiten, oder ein reines Sidescroller-Spiel entwickeln.</p>" + 
+                "<p>Wenn dir gefallen hat, was du hier gesehen hast, melde dich doch bei mir.</p>" +
+                "<p>Meine Email-Adresse lautet: <a href='mailto:email@lukas-roeding.de'>email@lukas-roeding.de</a></p>" +
+                "<p>Wenn du durch die nächste Tür gehst, landest du wieder am Ausgangslevel.</p>",
+                title: 'Fertig',
+                link: ''
+            }
         ],
         doors: [
             {
@@ -11,10 +23,10 @@ export function level(height) {
                 target: 'back'
             },
             {
-                x: height * 1.8,
+                x: height * 4.25,
                 y: height - height / 5,
                 target: '/'
-            },
+            }
         ],
         images: [                                        
         ],
@@ -70,11 +82,32 @@ export function level(height) {
                 spriteWidth: 32,
                 spriteHeight: 32,
                 spriteFrames: 8
+            },
+            {
+                id: 3,
+                x: height * 2.6,
+                y: height - height / 1.5 - height / 20,
+                w: height / 20,
+                h: height / 20,
+                sprites: {
+                    run: {
+                        left: createImage('../images/enemies/snail_run_left.png'),
+                        right: createImage('../images/enemies/snail_run_right.png')
+                    }
+                },
+                range: {
+                    leftX: height * 2.6,
+                    rightX: height * 2.8 - height / 20,
+                },
+                speed: 5,
+                spriteWidth: 38,
+                spriteHeight: 24,
+                spriteFrames: 9
             }
         ],
         platforms: [              
             {
-                id: 4,
+                id: 1,
                 x: -height / 2,
                 y: height - height / 10,
                 height: height / 10,
@@ -82,13 +115,21 @@ export function level(height) {
                 source: '../images/platform.png'
             },
             {
-                id: 4,
+                id: 2,
                 x: height * 1.5,
+                y: height - height / 5,
+                height: height / 5,
+                width: height / 2,
+                source: '../images/platformSmallTall.png'
+            },
+            {
+                id: 3,
+                x: height * 3.6,
                 y: height - height / 10,
                 height: height / 10,
                 width: height,
                 source: '../images/platform.png'
-            }
+            },
         ],
         blocks: [
             {
@@ -110,6 +151,38 @@ export function level(height) {
             {
                 id: 3,
                 x: height * 1.25,
+                y: height - height / 10,
+                height: height / 20,
+                width: height / 20,
+                source: '../images/block.png'
+            },
+            {
+                id: 4,
+                x: height * 2.6,
+                y: height - height / 1.5,
+                height: height / 5,
+                width: height / 5,
+                source: '../images/block.png'
+            },
+            {
+                id: 5,
+                x: height * 2.4,
+                y: height - height / 2,
+                height: height / 20,
+                width: height / 20,
+                source: '../images/block.png'
+            },
+            {
+                id: 6,
+                x: height * 2.2,
+                y: height - height / 2.5,
+                height: height / 20,
+                width: height / 20,
+                source: '../images/block.png'
+            },
+            {
+                id: 7,
+                x: height * 3.2,
                 y: height - height / 10,
                 height: height / 20,
                 width: height / 20,
