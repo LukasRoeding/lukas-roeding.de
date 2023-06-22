4af4c1fb29d745a6597a37c616eb8abc97fafa84
LukasRoeding <goskodovesche@gmail.com>
2023-06-22 14:48:23 +0200

    start of fun level

--- levels/fun_level.js
+++ levels/fun_level.js
@@ -3,28 +3,6 @@ import { createImage } from "../js/createImage.js";
 export function level(height) {
     return {
         informations: [
-            {
-                id: 1,
-                x: height / 2 - height / 5,
-                y: height - height / 3,
-                html:   "<p>In diesem Level erzähle ich ein bischen zu meiner Person.</p>" + 
-                        "<p>Welche Hobbies habe ich? Was mache ich ehrenamtlich? Wie ticke ich als Mensch?</p>",
-                title: 'Über mich',
-                link: ''
-            },
-            {
-                id: 2,
-                x: height - height / 5,
-                y: height - height / 1.6,
-                html:   "<p>Vom 01.09.2021 - 20.01.2023 war ich ein Azubi für den Fachinformatiker für Anwendungsentwicklung.</p>" + 
-                        '<p>Die "Regelstudienzeit" beträgt 3 Jahre, ich schaffte es die Ausbildung in 16 Monaten und 20 Tagen zu absolvieren.</p>' +
-                        '<p>Mein Abschlussprojekt lautete: Typo3 Nuxt 3 Skeleton.</p>' +
-                        '<p>Die Abschlussnote der Berufsschule war eine 2,0 (Zwischennote 1,25, bin gegen Ende nicht mehr hingegangen, weil ich tatsächlich was lernen wollte) und meine Abschlussnote der IHK war eine 3,7.</p>' +
-                        '<p>Leider litt meine Abschlussnote der IHK unter dem Fakt, dass ich 100% der Prüfungen in 4 Monaten geschrieben habe, während ich gleichzeitig umgezogen bin.</p>' + 
-                        '<p>Ich bereue das Verkürzen aber nicht, da man in der Berufsschule nicht wirklich was lernt und die IHK fast nichts sinnvolles prüft.</p>',
-                title: 'Fachinformatiker für Anwendungsentwicklung',
-                link: ''
-            },
         ],
         doors: [
             {
@@ -35,26 +13,10 @@ export function level(height) {
             {
                 x: height * 1.8,
                 y: height - height / 5,
-                target: '/fun.html'
+                target: '/'
             },
         ],
-        images: [    
-            {
-                id: 1,
-                x: height - height / 8 - 3,
-                y: height - height / 1.6,
-                w: height * 0.375,
-                h: height / 5,
-                source: '../images/education/ihk.jpg'
-            },                                        
-            {
-                id: 2,
-                x: height * 1.8,
-                y: height - height / 4 - height / 8,
-                w: height / 10,
-                h: height / 10,
-                source: '../images/fun.png'
-            },  
+        images: [                                        
         ],
         backgroundImages: [
             {
@@ -68,118 +30,89 @@ export function level(height) {
         ],
         enemies: [
             {
-                id: 0,
+                id: 2,
                 x: height / 2 + 2,
-                y: height - height / 4 - height / 20,
+                y: height - height / 4 - height / 10,
                 w: height / 20,
                 h: height / 20,
                 sprites: {
                     run: {
-                        left: createImage('../images/enemies/snail_run_left.png'),
-                        right: createImage('../images/enemies/snail_run_right.png')
+                        left: createImage('../images/enemies/bird_flying_left.png'),
+                        right: createImage('../images/enemies/bird_flying_right.png')
                     }
                 },
                 range: {
                     leftX: height / 2,
-                    rightX: height - height / 20,
+                    rightX: height * 1.45,
                 },
-                speed: 10,
-                spriteWidth: 38,
-                spriteHeight: 24,
-                spriteFrames: 9
-            },
-            {
-                id: 1,
-                x: height / 2 - height / 8 + 2,
-                y: height - height / 10 - height / 20,
-                w: height / 20,
-                h: height / 20,
-                sprites: {
-                    run: {
-                        left: createImage('../images/enemies/snail_run_left.png'),
-                        right: createImage('../images/enemies/snail_run_right.png')
-                    }
-                },
-                range: {
-                    leftX: height / 2 - height / 8,
-                    rightX: height * 1.5 + height / 8 - height / 20,
-                },
-                speed: 10,
-                spriteWidth: 38,
-                spriteHeight: 24,
-                spriteFrames: 9
+                speed: 2,
+                spriteWidth: 32,
+                spriteHeight: 32,
+                spriteFrames: 8
             },
             {
                 id: 2,
-                x: height * 0.75 + 2,
-                y: height - height / 4 - height / 6 - height / 20,
+                x: height,
+                y: height - height / 4 - height / 10,
                 w: height / 20,
                 h: height / 20,
                 sprites: {
                     run: {
-                        left: createImage('../images/enemies/snail_run_left.png'),
-                        right: createImage('../images/enemies/snail_run_right.png')
+                        left: createImage('../images/enemies/bird_flying_left.png'),
+                        right: createImage('../images/enemies/bird_flying_right.png')
                     }
                 },
                 range: {
-                    leftX: height * 0.75,
-                    rightX: height * 1.2,
+                    leftX: height / 2,
+                    rightX: height * 1.45,
                 },
-                speed: 10,
-                spriteWidth: 38,
-                spriteHeight: 24,
-                spriteFrames: 9
+                speed: 2,
+                spriteWidth: 32,
+                spriteHeight: 32,
+                spriteFrames: 8
             }
         ],
         platforms: [              
             {
-                id: 1,
-                x: height - height / 4 - 2,
-                y: height - height / 4 - height / 6,
-                height: height / 4,
-                width: height / 2,
-                source: '../images/platformSmallTall.png'
-            },      
-            {
-                id: 2,
-                x: height / 2 - 2,
-                y: height - height / 4,
-                height: height / 4,
-                width: height / 2,
-                source: '../images/platformSmallTall.png'
-            },
-            {
-                id: 3,
-                x: height - 3,
-                y: height - height / 4,
-                height: height / 4,
-                width: height / 2,
-                source: '../images/platformSmallTall.png'
+                id: 4,
+                x: -height / 2,
+                y: height - height / 10,
+                height: height / 10,
+                width: height,
+                source: '../images/platform.png'
             },
             {
                 id: 4,
-                x: -2,
+                x: height * 1.5,
                 y: height - height / 10,
                 height: height / 10,
                 width: height,
                 source: '../images/platform.png'
-            },
-            {
-                id: 5,
-                x: height - 3,
-                y: height - height / 10,
-                height: height / 10,
-                width: height,
-                source: '../images/platform.png'
-            },
+            }
         ],
         blocks: [
             {
                 id: 1,
-                x: height / 2,
-                y: height / 2,
-                height: height / 10,
-                width: height / 10,
+                x: height * 0.75,
+                y: height - height / 10,
+                height: height / 20,
+                width: height / 20,
+                source: '../images/block.png'
+            },
+            {
+                id: 2,
+                x: height * 1,
+                y: height - height / 10,
+                height: height / 20,
+                width: height / 20,
+                source: '../images/block.png'
+            },
+            {
+                id: 3,
+                x: height * 1.25,
+                y: height - height / 10,
+                height: height / 20,
+                width: height / 20,
                 source: '../images/block.png'
             },
         ]
