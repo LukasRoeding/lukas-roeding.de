0594bc5f8faecc24eeafc999edb1507115f14165
LukasRoeding <goskodovesche@gmail.com>
2023-12-09 22:52:34 +0100

    importing sql data and obviously not pushing it to git

--- backend/index.php
+++ backend/index.php
@@ -1,5 +1,7 @@
 <?php
 header("Access-Control-Allow-Origin: http://http://127.0.0.1:5500");
+include 'sql.php';
+
 if($_SERVER['REQUEST_METHOD'] === 'POST') {
     echo 'Hello ' . htmlspecialchars($_POST["name"]) . '!';
 }
