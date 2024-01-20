dedbf376da01c82882b8dc9070ffa8733f323db0
Lukas <goskodovesche@gmail.com>
2024-01-20 10:56:04 +0100

    removing stuff because hackathon

--- backend/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-header("Access-Control-Allow-Origin: http://http://127.0.0.1:5500");
-include 'sql.php';
-
-if($_SERVER['REQUEST_METHOD'] === 'POST') {
-    echo 'Hello ' . htmlspecialchars($_POST["name"]) . '!';
-}
-?>
